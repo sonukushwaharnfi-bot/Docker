211484269306a326acdcb74047eeefb6e3b6dc36
Sonu Kushwaha <sonukushwaharnfi@gmail.com>
2026-09-17 11:41:18 +0530

    Restore deleted Docker practice files

---
+++ app-with-db/app.py
@@ -0,0 +1,1 @@
+print("Hello! Mera app chal raha hai, MySQL ke saath connected hone ke liye ready hai!")
---
+++ python-app/app.py
@@ -0,0 +1,2 @@
+print("Hello! Ye mera Python app hai, Docker ke andar chal raha hai!")
+print("Sonu Kushwaha - DevOps Journey")
